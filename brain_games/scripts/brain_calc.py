9ae06993207a596268937b0f1588ef7c769293e0
LyuPo <lport@frtk.ru>
2020-02-21 09:53:15 +0300

    Update brain_even

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,9 @@
-"""Brain-even function."""
+"""Brain-calc function."""
 # This Python file uses the following encoding: utf-8
 import random
 
 
-def ask_user(name_player):
+def ask_user_calc(name_player):
     """Ask user and return user's and correct answers."""
     number = random.randint(0, 100)
     print('Question: ', number)
@@ -16,24 +16,20 @@ def ask_user(name_player):
     return (correct_answer, user_answer)
 
 
-def brain_even(name_player):
-    """Ask user if the number is even."""
-    print('Answer \"yes\" if number even otherwise answer \"no\"')
+def brain_calc(name_player):
+    """Ask user the result of the expression."""
+    print('What is the result of the expression?')
     init_game = 1
-    correct_answer = 'yes'
-    user_answer = 'no'
-    intent_number = 1
+    correct_answer = 1
+    user_answer = 0
 
-    while (((user_answer == correct_answer) or init_game) and intent_number <= 3):
-        correct_answer, user_answer = ask_user(name_player)
+    while ((user_answer == correct_answer) or init_game):
+        correct_answer, user_answer = ask_user_calc(name_player)
 
         if (user_answer == correct_answer):
             print('Correct!')
             init_game = 0
-            if (intent_number == 3):
-                print('Congratulations, ' + name_player + '!')
         else:
             print(user_answer, ' is wrong answer ;(. Correct answer was ', 
             correct_answer)
-        
-        intent_number += 1
+
